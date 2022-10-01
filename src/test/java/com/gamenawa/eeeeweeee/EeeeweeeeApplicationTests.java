e179ca81f46cc2f93af8aa7e341f01b1337058f4
Jonghyun <jhyun5717@gmail.com>
2022-10-01 13:02:43 +0900

    init

---
+++ src/test/java/com/gamenawa/eeeeweeee/EeeeweeeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamenawa.eeeeweeee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EeeeweeeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
