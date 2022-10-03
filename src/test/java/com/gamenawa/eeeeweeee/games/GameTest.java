3128dc190ea1481c2f21ff97ed3a8448b5378f33
Jonghyun <jhyun5717@gmail.com>
2022-10-03 19:53:55 +0900

    test: add Game and GameScore tests

---
+++ src/test/java/com/gamenawa/eeeeweeee/games/GameTest.java
@@ -0,0 +1,52 @@
+package com.gamenawa.eeeeweeee.games;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+    private Game game;
+
+    @BeforeEach
+    public void beforeEach() {
+        game = new Game("testTitle", "testGenre", "testDev", 1996);
+    }
+
+    @Test
+    void addScore() {
+        game.addScore(new GameScore("40", "hate"));
+        game.addScore(new GameScore("80", "like"));
+        assertThat(game.getScores().get(0).getScore()).isEqualTo("40");
+        assertThat(game.getScores().get(0).getRater()).isEqualTo("hate");
+        assertThat(game.getScores().get(1).getScore()).isEqualTo("80");
+        assertThat(game.getScores().get(1).getRater()).isEqualTo("like");
+    }
+
+    @Test
+    void getTitle() {
+        assertThat(game.getTitle()).isEqualTo("testTitle");
+    }
+
+    @Test
+    void getGenre() {
+        assertThat(game.getGenre()).isEqualTo("testGenre");
+    }
+
+    @Test
+    void getDeveloper() {
+        assertThat(game.getDeveloper()).isEqualTo("testDev");
+    }
+
+    @Test
+    void getReleaseYear() {
+        assertThat(game.getReleaseYear()).isEqualTo(1996);
+    }
+
+    @Test
+    void getScores() {
+        game.addScore(new GameScore("40", "hate"));
+        game.addScore(new GameScore("80", "like"));
+        assertThat(game.getScores().size()).isEqualTo(2);
+    }
+}
