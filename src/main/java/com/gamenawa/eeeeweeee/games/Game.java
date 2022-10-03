e2398c2bdbf49c462bd25997e4d077a2dad3beed
Jonghyun <jhyun5717@gmail.com>
2022-10-03 19:53:17 +0900

    feat: add Game and GameScore VO

---
+++ src/main/java/com/gamenawa/eeeeweeee/games/Game.java
@@ -0,0 +1,21 @@
+package com.gamenawa.eeeeweeee.games;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class Game {
+    private final String title;
+    private final String genre;
+    private final String developer;
+    private final Integer releaseYear;
+    private final List<GameScore> scores = new ArrayList<>();
+
+    public void addScore(GameScore score) {
+        this.scores.add(score);
+    }
+}
---
+++ src/main/java/com/gamenawa/eeeeweeee/games/GameScore.java
@@ -0,0 +1,11 @@
+package com.gamenawa.eeeeweeee.games;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@RequiredArgsConstructor
+public class GameScore {
+    private final String score;
+    private final String rater;
+}
